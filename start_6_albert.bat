a5af540136b5647406172ce108da202648fb4d1b
SiddarGu <fenggu2000@gmail.com>
2025-01-15 13:10:03 -0500

    script to determine num instances to run on command line

--- start_6_albert.bat
+++ start_6_albert.bat
@@ -1,4 +1,9 @@
 @echo off
-for /l %%x in (1, 1, 7) do (
+if "%~1"=="" (
+    echo Usage: %~nx0 [number_of_instances]
+    exit /b 1
+)
+
+for /l %%x in (1, 1, %1) do (
     start "" "C:\Program Files (x86)\daide\albert\Albert.exe"
 )
